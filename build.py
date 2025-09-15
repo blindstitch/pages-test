f7d6b1f755d1f65ce31a7d2f38e203eccd820ded
Karl T <karltacheron@gmail.com>
2025-09-15 16:49:38 -0400

    Update build.py

--- build.py
+++ build.py
@@ -2,5 +2,5 @@ from datetime import datetime
 now = datetime.now().strftime("%H:%M:%S")
 
 outfile = open('index.html','w')
-outfile.write('foo '+now)
+outfile.write('foo karls cool'+now)
 outfile.close()
