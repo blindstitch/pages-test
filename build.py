e641a1a0924c65470d74e0b32f5204c880932de6
Karl T <karltacheron@gmail.com>
2025-09-16 13:06:25 -0400

    timezone

--- build.py
+++ build.py
@@ -1,5 +1,7 @@
 from datetime import datetime
-now = datetime.now().strftime("%B %d %Y %H:%M:%S")
+import zoneinfo
+
+now = datetime.now(zoneinfo.ZoneInfo("America/New_York")).strftime("%B %d %Y %H:%M:%S")
 
 outfile = open('index.html','w')
 outfile.write('Hello! This page last built automatically by a Github Pages workflow at: '+now)
