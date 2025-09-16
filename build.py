849d3d7226f8207474cc8dd365d2001590fc0e73
Karl T <karltacheron@gmail.com>
2025-09-16 13:10:31 -0400

    Update build.py

--- build.py
+++ build.py
@@ -8,6 +8,8 @@ Hello! This page last built automatically by a Github Pages workflow at: {now}
 <br>
 <br>
 See the repository at <a href="https://github.com/blindstitch/pages-test">https://github.com/blindstitch/pages-test</a> for page source.
+'''
+
 outfile = open('index.html','w')
 outfile.write(')
 outfile.close()
