1e418239ee1a669f3419fe63dc80cadfa2d274b7
Karl T <karltacheron@gmail.com>
2025-09-16 13:09:52 -0400

    Update build.py

--- build.py
+++ build.py
@@ -3,6 +3,11 @@ import zoneinfo
 
 now = datetime.now(zoneinfo.ZoneInfo("America/New_York")).strftime("%B %d %Y %H:%M:%S")
 
+out = f'''
+Hello! This page last built automatically by a Github Pages workflow at: {now}
+<br>
+<br>
+See the repository at <a href="https://github.com/blindstitch/pages-test">https://github.com/blindstitch/pages-test</a> for page source.
 outfile = open('index.html','w')
-outfile.write('Hello! This page last built automatically by a Github Pages workflow at: '+now)
+outfile.write(')
 outfile.close()
